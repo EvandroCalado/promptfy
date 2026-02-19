5067b0140ce8e7e9f27238637e2fbcf6b145258c
Evandro Calado <evandrocalado@gmail.com>
2026-02-19 10:43:13 -0300

    chore: initial setup and db

---
+++ prisma/migrations/20260219132521_create_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "prompts" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "prompts_pkey" PRIMARY KEY ("id")
+);
