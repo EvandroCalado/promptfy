c3b7ebdd62aff9cbfaeb11665fe01a5a0797127a
Evandro Calado <evandrocalado@gmail.com>
2026-02-24 18:46:12 -0300

    test: add navigation test for 'New prompt' button in SidebarContent.

--- src/tests/components/shared/sidebar-content.spec.tsx
+++ src/tests/components/shared/sidebar-content.spec.tsx
@@ -1,9 +1,11 @@
 import { SidebarContent } from '@/components/shared/sidebar-content';
 import { fireEvent, render, screen } from '@/lib/test-utils';
 
+const pushMock = jest.fn();
+
 jest.mock('next/navigation', () => ({
   useRouter: () => ({
-    push: jest.fn(),
+    push: pushMock,
   }),
 }));
 
@@ -33,5 +35,15 @@ describe('SidebarContent', () => {
     expect(sidebar).toHaveClass('md:w-22');
     expect(screen.getByText('New prompt')).toHaveClass('opacity-0');
     expect(screen.getByText('New prompt')).not.toHaveClass('opacity-100');
+  });
+
+  it('should navidate to new prompt page', () => {
+    makeSut();
+
+    const button = screen.getByRole('button', { name: /new prompt/i });
+
+    fireEvent.click(button);
+
+    expect(pushMock).toHaveBeenCalledWith('/new');
   });
 });
